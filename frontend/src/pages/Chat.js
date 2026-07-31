4982b500f4781dce34ac089c96f40ae9b4b7a340
IshithaReddy29 <ishithareddyr@gmail.com>
2026-07-31 20:59:35 +0530

    useref

--- frontend/src/pages/Chat.js
+++ frontend/src/pages/Chat.js
@@ -1,4 +1,4 @@
-import { useRef } from "react";
+
 import { useState, useEffect, useRef, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import { FaPaperPlane, FaSignOutAlt, FaUserCircle } from "react-icons/fa";
